a485849f50c6343cf52c5b57e676ea2fcbfa4206
Andrei Poturaev <andrei.poturaev@netcracker.com>
2017-09-27 12:13:29 +0300

    dsc configuration

--- rollout.sh
+++ rollout.sh
@@ -10,3 +10,9 @@ az storage blob upload -f $DISTR_ZIP -c $STORAGE_CONTAINER -n $DISTR_ZIP
 rm -f $DISTR_ZIP
 #infrastructure rollout
 . iaas_preparation.sh
+#prepare config
+. prepare_config.sh
+#upload server config
+echo "upload server config"
+az storage blob upload -f $CONFIG_ZIP -c $STORAGE_CONTAINER -n $CONFIG_ZIP
+rm -f $CONFIG_ZIP
